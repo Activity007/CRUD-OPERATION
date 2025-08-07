8f6294928bff131df928019c891743f7b9893fc0
Activity007 <tithcholna742@gmail.com>
2025-08-07 23:24:47 +0700

    Seconde commit

--- src/User.java
+++ src/User.java
@@ -9,16 +9,4 @@ public class User {
     private Integer id;
     private String name;
     private Integer age;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
 }
